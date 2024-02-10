bb679a39fee59c01fef674c990ca5a54b9032e7a
calagkit100 <calagkit100@gmail.com>
2024-02-10 12:01:42 +0800

    partial update

--- main.js
+++ main.js
@@ -42,11 +42,10 @@ $(document).ready(function () {
     $("#confirmPassword").click(function () {
 
         let inputPwd = $("#inputPassword").val();
-        console.log('inputpasss', inputPwd);
-        console.log('password', password);
+        // console.log('inputpasss', inputPwd);
+        // console.log('password', password);
         if (inputPwd == password) {
             $("#redirectFlower a")[0].click();
-            ("#myAudio")[0].play();
         } else {
             $("#promptMSG").text('Wrong password');
         }
